702b49796d6e1e7b7532906b9bc8ae624823cc54
ana-aranha <anab.aranha@gmail.com>
2022-10-20 22:41:23 -0300

    refator: column name from users table

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -2,7 +2,7 @@ import connection from "../db/database.js";
 
 async function getPosts() {
 	const posts = await connection.query(
-		`SELECT user.id, users.username, users.picture, posts.text, posts.url FROM posts JOIN users ON posts."userId" = users.id WHERE posts."deletedAt" IS NULL ORDER BY posts."createdAt" DESC LIMIT 20;`,
+		`SELECT user.id, users.name, users.picture, posts.text, posts.url FROM posts JOIN users ON posts."userId" = users.id WHERE posts."deletedAt" IS NULL ORDER BY posts."createdAt" DESC LIMIT 20;`,
 	);
 
 	return posts;
