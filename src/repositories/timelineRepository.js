77a0fce991e236d4eec91e6d24f6e183ddaf4a37
tamuzeus <igortamuz@gmail.com>
2022-10-26 12:12:08 -0300

    Remove: limit in post qnt

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -2,7 +2,7 @@ import connection from "../db/database.js";
 
 async function getPosts(userId) {
 	const posts = await connection.query(
-		`SELECT users.id AS "userId", users.name, users.picture, posts.id AS "postId", posts.text, posts.url, posts."userId" = ${userId} AS owner, a."postId" = "postId" AS liked FROM posts JOIN users ON posts."userId" = users.id LEFT JOIN (SELECT "postId" FROM likes WHERE likes."userId" = ${userId}) AS a ON a."postId" = posts.id WHERE posts."deletedAt" IS NULL ORDER BY posts."createdAt" DESC LIMIT 20;`,
+		`SELECT users.id AS "userId", users.name, users.picture, posts.id AS "postId", posts.text, posts.url, posts."userId" = ${userId} AS owner, a."postId" = "postId" AS liked FROM posts JOIN users ON posts."userId" = users.id LEFT JOIN (SELECT "postId" FROM likes WHERE likes."userId" = ${userId}) AS a ON a."postId" = posts.id WHERE posts."deletedAt" IS NULL ORDER BY posts."createdAt" DESC;`,
 	);
 
 	return posts;
